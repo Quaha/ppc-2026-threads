d70c296591bb81b210ad0fdf96d9ecaf4b34ee62
TiClass322 <dana.kopilov@gmail.com>
2026-03-21 09:46:18 +0300

    omp3_clang

--- tasks/kopilov_d_vertical_gauss_filter/tests/performance/main.cpp
+++ tasks/kopilov_d_vertical_gauss_filter/tests/performance/main.cpp
@@ -9,53 +9,53 @@
 #include "kopilov_d_vertical_gauss_filter/seq/include/ops_seq.hpp"
 #include "util/include/perf_test_util.hpp"
 
-        namespace kopilov_d_vertical_gauss_filter {
+namespace kopilov_d_vertical_gauss_filter {
 
-  class KopilovDVerticalGaussFilterPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
-    static constexpr int kWidth = 8192;
-    static constexpr int kHeight = 8192;
-    InType input_data_{};
+class KopilovDVerticalGaussFilterPerfTests : public ppc::util::BaseRunPerfTests<InType, OutType> {
+  static constexpr int kWidth = 8192;
+  static constexpr int kHeight = 8192;
+  InType input_data_{};
 
-    void SetUp() override {
-      std::random_device rd;
-      std::mt19937 gen(rd());
-      std::uniform_int_distribution<int> dist(0, 255);
+  void SetUp() override {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(0, 255);
 
-      input_data_.width = kWidth;
-      input_data_.height = kHeight;
+    input_data_.width = kWidth;
+    input_data_.height = kHeight;
 
-      input_data_.data.resize(static_cast<size_t>(kWidth) * static_cast<size_t>(kHeight));
-      for (auto &val : input_data_.data) {
-        val = static_cast<std::uint8_t>(dist(gen));
-      }
+    input_data_.data.resize(static_cast<size_t>(kWidth) * static_cast<size_t>(kHeight));
+    for (auto &val : input_data_.data) {
+      val = static_cast<std::uint8_t>(dist(gen));
     }
-
-    bool CheckTestOutputData(OutType &output_data) final {
-      return output_data.width == input_data_.width && output_data.height == input_data_.height &&
-             output_data.data.size() == input_data_.data.size();
-    }
-
-    InType GetTestInputData() final {
-      return input_data_;
-    }
-  };
-
-  TEST_P(KopilovDVerticalGaussFilterPerfTests, RunPerfModes) {
-    ExecuteTest(GetParam());
   }
 
-  namespace {
+  bool CheckTestOutputData(OutType &output_data) final {
+    return output_data.width == input_data_.width && output_data.height == input_data_.height &&
+           output_data.data.size() == input_data_.data.size();
+  }
 
-  const auto kAllPerfTasks =
-      ppc::util::MakeAllPerfTasks<InType, KopilovDVerticalGaussFilterSEQ, KopilovDVerticalGaussFilterOMP>(
-          PPC_SETTINGS_kopilov_d_vertical_gauss_filter);
+  InType GetTestInputData() final {
+    return input_data_;
+  }
+};
 
-  const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+TEST_P(KopilovDVerticalGaussFilterPerfTests, RunPerfModes) {
+  ExecuteTest(GetParam());
+}
 
-  const auto kPerfTestName = KopilovDVerticalGaussFilterPerfTests::CustomPerfTestName;
+namespace {
 
-  INSTANTIATE_TEST_SUITE_P(RunModeTests, KopilovDVerticalGaussFilterPerfTests, kGtestValues, kPerfTestName);
+const auto kAllPerfTasks =
+    ppc::util::MakeAllPerfTasks<InType, KopilovDVerticalGaussFilterSEQ, KopilovDVerticalGaussFilterOMP>(
+        PPC_SETTINGS_kopilov_d_vertical_gauss_filter);
 
-  }  // namespace
+const auto kGtestValues = ppc::util::TupleToGTestValues(kAllPerfTasks);
+
+const auto kPerfTestName = KopilovDVerticalGaussFilterPerfTests::CustomPerfTestName;
+
+INSTANTIATE_TEST_SUITE_P(RunModeTests, KopilovDVerticalGaussFilterPerfTests, kGtestValues, kPerfTestName);
+
+}  // namespace
 
 }  // namespace kopilov_d_vertical_gauss_filter
